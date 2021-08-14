fee0f50571bb2ce114c81faef7cfd2be42ad7c97
goldwinalec <a-g1997@yandex.ru>
2021-08-14 12:06:34 +0500

    working on media.css

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const toggleNav = document.querySelector(".header__nav-toggle");
 const header = document.querySelector(".header");
 const headerNav = document.querySelector(".header__nav");
 const html = document.querySelector("html");
-const navLink = document.querySelectorAll("header__nav-link");
+const navLinks = document.querySelectorAll("header__nav-link");
 const footerCopyright = document.querySelector(".footer__copyright");
 
 toggleNav.addEventListener("click", function () {
@@ -13,4 +13,37 @@ toggleNav.addEventListener("click", function () {
   headerNav.classList.toggle("header__nav--active");
   footerCopyright.classList.toggle("footer__copyright--active");
   html.classList.toggle("html--disabled");
+});
+
+$(document).on("click", ".header__nav-link", function () {
+  var linkID = $(this).attr("href");
+  $("html, body").animate(
+    {
+      scrollTop: $(linkID).offset().top,
+    },
+    "slow"
+  );
+  toggleNav.classList.remove("header__nav-toggle--active");
+  header.classList.remove("header--active");
+  headerNav.classList.remove("header__nav--active");
+  footerCopyright.classList.remove("footer__copyright--active");
+  html.classList.remove("html--disabled");
+});
+$(document).on("click", ".hero__link", function () {
+  var linkID = $(this).attr("href");
+  $("html, body").animate(
+    {
+      scrollTop: $(linkID).offset().top,
+    },
+    "slow"
+  );
+});
+$(document).on("click", ".footer__link", function () {
+  var linkID = $(this).attr("href");
+  $("html, body").animate(
+    {
+      scrollTop: $(linkID).offset().top,
+    },
+    "slow"
+  );
 });
