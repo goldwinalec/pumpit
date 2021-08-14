96a63632e069a289ece48dc5b28db6ec6d8809a0
goldwinalec <a-g1997@yandex.ru>
2021-08-14 11:43:48 +0500

    working on media.css

--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@ const toggleNav = document.querySelector(".header__nav-toggle");
 const header = document.querySelector(".header");
 const headerNav = document.querySelector(".header__nav");
 const html = document.querySelector("html");
+const navLink = document.querySelectorAll("header__nav-link");
+const footerCopyright = document.querySelector(".footer__copyright");
 
 toggleNav.addEventListener("click", function () {
   toggleNav.classList.toggle("header__nav-toggle--active");
   header.classList.toggle("header--active");
   headerNav.classList.toggle("header__nav--active");
+  footerCopyright.classList.toggle("footer__copyright--active");
   html.classList.toggle("html--disabled");
 });
