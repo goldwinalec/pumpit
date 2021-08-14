d56b4f70c93fe1827eea54ca17335a6b0ca5bcab
goldwinalec <a-g1997@yandex.ru>
2021-08-14 12:15:32 +0500

    working on media.css

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const toggleNav = document.querySelector(".header__nav-toggle");
 const header = document.querySelector(".header");
 const headerNav = document.querySelector(".header__nav");
 const html = document.querySelector("html");
-const navLinks = document.querySelectorAll("header__nav-link");
+const navLink = document.querySelectorAll(".header__nav-link");
 const footerCopyright = document.querySelector(".footer__copyright");
 
 toggleNav.addEventListener("click", function () {
@@ -15,35 +15,21 @@ toggleNav.addEventListener("click", function () {
   html.classList.toggle("html--disabled");
 });
 
-$(document).on("click", ".header__nav-link", function () {
-  var linkID = $(this).attr("href");
-  $("html, body").animate(
-    {
-      scrollTop: $(linkID).offset().top,
-    },
-    "slow"
-  );
-  toggleNav.classList.remove("header__nav-toggle--active");
-  header.classList.remove("header--active");
-  headerNav.classList.remove("header__nav--active");
-  footerCopyright.classList.remove("footer__copyright--active");
-  html.classList.remove("html--disabled");
+navLink.forEach((link) => {
+  link.addEventListener("click", function () {
+    toggleNav.classList.remove("header__nav-toggle--active");
+    header.classList.remove("header--active");
+    headerNav.classList.remove("header__nav--active");
+    footerCopyright.classList.remove("footer__copyright--active");
+    html.classList.remove("html--disabled");
+  });
 });
-$(document).on("click", ".hero__link", function () {
-  var linkID = $(this).attr("href");
-  $("html, body").animate(
-    {
-      scrollTop: $(linkID).offset().top,
-    },
-    "slow"
-  );
-});
-$(document).on("click", ".footer__link", function () {
-  var linkID = $(this).attr("href");
-  $("html, body").animate(
-    {
-      scrollTop: $(linkID).offset().top,
-    },
-    "slow"
-  );
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
